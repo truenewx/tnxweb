dcf3dbe5c4249c2efcdc9bb1d93a0a8d50934a0c
jiangnanyuzi <jiangnanyuzi@163.com>
2019-12-29 10:24:50 +0800

    优化跨域访问

--- src/main/java/org/truenewx/tnxweb/filter/CorsFilter.java
+++ src/main/java/org/truenewx/tnxweb/filter/CorsFilter.java
@@ -1,13 +1,20 @@
 package org.truenewx.tnxweb.filter;
 
-import javax.servlet.*;
-import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * CORS跨域处理过滤器
  */
 public class CorsFilter implements Filter {
+
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
     }
@@ -17,13 +24,12 @@ public class CorsFilter implements Filter {
             throws IOException, ServletException {
         HttpServletResponse httpResponse = (HttpServletResponse) response;
         httpResponse.setHeader("Access-Control-Allow-Origin", "*");
-        httpResponse.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
         httpResponse.setHeader("Access-Control-Allow-Headers", "x-requested-with,Authorization");
-        httpResponse.setHeader("Access-Control-Allow-Credentials", "true");
         chain.doFilter(request, httpResponse);
     }
 
     @Override
     public void destroy() {
     }
+
 }
