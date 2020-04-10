7518bc2e5eb51f529b57b9f060e591ebe8451100
jiangnanyuzi <jiangnanyuzi@163.com>
2020-04-10 18:00:21 +0800

    加入Vuelidate校验框架

--- src/main/webapp/vue/tnx/js/tnxvue.js
+++ src/main/webapp/vue/tnx/js/tnxvue.js
@@ -1,5 +1,5 @@
 // tnxvue.js
-var components = {
+var vue_components = {
     "tv-div": app_config.lib + "/vue/component/div",
     "tv-span": app_config.lib + "/vue/component/span",
 };
@@ -8,17 +8,31 @@ require.config({
         "tnxcore": app_config.lib + "/core/tnx/js/tnxcore",
         "vue": app_config.lib + "/vue/vendor/vue-2.6.10/vue" + app_config.min,
         "vue-router": app_config.lib + "/vue/vendor/vue-router-3.1.3/vue-router" + app_config.min,
-    }, components)
+        "vuelidate": app_config.lib + "/vue/vendor/vuelidate-0.7.5/vuelidate.min",
+        "vuelidate-rules": app_config.lib + "/vue/vendor/vuelidate-0.7.5/validators.min",
+    }, vue_components)
 });
 
 var Vue;
 var VueRouter;
+var Vuelidate;
 
-define(["tnxcore", "vue", "vue-router"], function(tnxcore, v, vr) {
+define(["tnxcore", "vue", "vue-router", "vuelidate", "vuelidate-rules"], function(tnxcore, v, vr, vl, rules) {
     Vue = v;
     VueRouter = vr;
-    require(Object.keys(components));
+    Vuelidate = vl;
+    require(Object.keys(vue_components));
     Vue.use(VueRouter);
+    Vue.use(Vuelidate);
+    Vuelidate.rules = rules;
+    Vuelidate.transfer = function(meta) {
+        var $vModel = {};
+        Object.keys(meta).forEach(function(fieldName) {
+
+        });
+        return $vModel;
+    }
+
     tnxcore.util.initPage = Function.around(tnxcore.util.initPage, function(initPage, page, container) {
         if (container.tagName == "BODY") { // vue不支持以body为容器，故从body下获取第一个div作为容器
             for (var i = 0; i < container.children.length; i++) {
