52f3f361213ab78b8ed6396e318a15e00896179c
jianglei81 <jianglei81@sina.cn>
2020-08-22 19:45:43 +0800

    完成标签选择器基本功能

--- components/vue/element/tag-select/TagSelect.vue
+++ components/vue/element/tag-select/TagSelect.vue
@@ -1,9 +1,13 @@
 <template>
-    <div class="tnxel-tag-group">
-        <el-tag v-for="(item, index) in items" :key="item.key" :type="type" :size="size"
-            :effect="item.selected ? 'dark' : 'plain'" @click="select(index)">
-            {{item.text || item.caption || item.label}}
+    <div class="tnxel-tag-group" v-if="tags">
+        <el-tag v-for="(tag, index) in tags"
+            :key="tag.key" :type="type" :size="size"
+            :effect="tag.selected ? 'dark' : 'plain'" @click="select(index)">
+            {{tag.text}}
         </el-tag>
+    </div>
+    <div v-else>
+        <i class="el-icon-loading"/>
     </div>
 </template>
 
@@ -17,6 +21,14 @@ export default {
             type: Array,
             required: true,
         },
+        keyName: {
+            type: String,
+            default: 'key',
+        },
+        textName: {
+            type: String,
+            default: 'text',
+        },
         keys: {
             type: Array,
             default() {
@@ -24,9 +36,44 @@ export default {
             }
         },
     },
+    data() {
+        return {
+            tags: this.getTags()
+        }
+    },
+    watch: {
+        items() {
+            this.tags = this.getTags();
+        }
+    },
     methods: {
+        getTags() {
+            if (this.items && this.items.length) {
+                const vm = this;
+                const tags = [];
+                this.items.forEach(item => {
+                    const key = item[vm.keyName];
+                    tags.push({
+                        key: key,
+                        text: item[vm.textName],
+                        selected: vm.keys.contains(key),
+                    });
+                });
+                return tags;
+            }
+            return undefined;
+        },
         select(index) {
-
+            this.tags[index].selected = !this.tags[index].selected;
+        },
+        getSelectedKeys() {
+            const keys = [];
+            this.tags.forEach(tag => {
+                if (tag.selected) {
+                    keys.push(tag.key);
+                }
+            });
+            return keys;
         }
     }
 }
