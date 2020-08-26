15b6099826019133fa405e47775a744fd8ea7a74
jianglei <jianglei81@sina.cn>
2020-08-26 21:21:53 +0800

    解决登录后不能回到上一个页面的问题

--- components/tnxcore-app-rpc.js
+++ components/tnxcore-app-rpc.js
@@ -5,7 +5,7 @@ import axios from "axios";
 export default {
     loginSuccessRedirectParameter: '_next',
     apps: {},
-    getBaseUrl () {
+    getBaseUrl() {
         return axios.defaults.baseURL;
     },
     /**
@@ -13,7 +13,7 @@ export default {
      * @param baseUrl 获取配置的后端服务器基础路径
      * @param callback 配置初始化后的回调函数
      */
-    loadConfig (baseUrl, callback) {
+    loadConfig(baseUrl, callback) {
         if (typeof baseUrl === 'function') {
             callback = baseUrl;
             baseUrl = undefined;
@@ -29,7 +29,7 @@ export default {
             }
         });
     },
-    setConfig (config) {
+    setConfig(config) {
         if (config.baseUrl) {
             axios.defaults.baseURL = config.baseUrl;
         }
@@ -48,7 +48,7 @@ export default {
             'X-Requested-With': 'XMLHttpRequest'
         });
     },
-    get (url, params, callback, options) {
+    get(url, params, callback, options) {
         if (typeof params === 'function' || (callback && typeof callback === 'object')) {
             options = callback;
             callback = params;
@@ -65,7 +65,7 @@ export default {
             success: callback,
         }));
     },
-    post (url, body, callback, options) {
+    post(url, body, callback, options) {
         if (typeof body === 'function' || (callback && typeof callback === 'object')) {
             options = callback;
             callback = body;
@@ -83,7 +83,7 @@ export default {
         });
         this.request(url, options);
     },
-    request (url, options) {
+    request(url, options) {
         if (options.base) {
             const baseUrl = this.apps[options.base];
             if (baseUrl) {
@@ -109,7 +109,7 @@ export default {
         }
         this._request(url, config, options);
     },
-    _request (url, config, options) {
+    _request(url, config, options) {
         const _this = this;
         axios(url, config).then(function(response) {
             if (_this._redirectRequest(response, config, options)) { // 执行了重定向跳转，则不作后续处理
@@ -132,7 +132,8 @@ export default {
                         let loginUrl = util.getHeader(response.headers, 'Login-Url');
                         if (loginUrl) {
                             // 默认登录后跳转回当前页面
-                            loginUrl += '&' + _this.loginSuccessRedirectParameter + '=' + window.location.href;
+                            const loginSuccessRedirectUrl = encodeURIComponent(window.location.href);
+                            loginUrl += '&' + _this.loginSuccessRedirectParameter + '=' + loginSuccessRedirectUrl;
                         }
                         const originalRequest = util.getHeader(response.headers, 'Original-Request');
                         let originalMethod;
@@ -179,7 +180,7 @@ export default {
             console.error(url + ':\n' + error.stack);
         });
     },
-    _redirectRequest (response, config, options) {
+    _redirectRequest(response, config, options) {
         let redirectUrl = util.getHeader(response.headers, 'Redirect-To');
         if (redirectUrl) { // 指定了重定向地址，则执行重定向操作
             if (this._isIgnored(options, 'Redirect-To')) {
@@ -193,7 +194,7 @@ export default {
         }
         return false;
     },
-    _isIgnored (options, type) {
+    _isIgnored(options, type) {
         if (options && options.ignored) {
             if (options.ignored instanceof Array) {
                 return options.ignored.contains(type);
@@ -211,10 +212,10 @@ export default {
      * @param originalMethod 原始请求方法
      * @returns {boolean} 是否已经正常打开登录表单
      */
-    toLogin (loginFormUrl, originalUrl, originalMethod) {
+    toLogin(loginFormUrl, originalUrl, originalMethod) {
         return false;
     },
-    _handleErrors (errors, options) {
+    _handleErrors(errors, options) {
         if (errors) {
             if (options && typeof options.error === 'function') {
                 options.error(errors);
@@ -225,11 +226,11 @@ export default {
         }
         return false;
     },
-    error (errors) {
+    error(errors) {
         const message = this.getErrorMessage(errors);
         window.tnx.error(message);
     },
-    getErrorMessage (errors) {
+    getErrorMessage(errors) {
         let message = '';
         if (errors instanceof Array) {
             for (let i = 0; i < errors.length; i++) {
@@ -239,11 +240,11 @@ export default {
         return message.trim();
     },
     _ensureLoginedUrl: '/authentication/validate',
-    ensureLogined (callback, options) {
+    ensureLogined(callback, options) {
         this.get(this._ensureLoginedUrl, callback, options);
     },
     _metas: {},
-    getMeta (url, callback) {
+    getMeta(url, callback) {
         const metas = this._metas;
         if (metas[url]) {
             if (typeof callback === 'function') {
