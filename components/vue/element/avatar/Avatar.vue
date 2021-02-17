073044da451fc8cfe3cfb4c541554a35d38feefd
jianglei <jianglei81@sina.cn>
2021-02-17 13:47:15 +0800

    头像组件添加对fss存储路径的支持

--- components/vue/element/avatar/Avatar.vue
+++ components/vue/element/avatar/Avatar.vue
@@ -1,24 +1,73 @@
 <template>
-    <el-avatar :size="size" :style="style" icon="el-icon-user-solid" :shape="shape" fit="contain" :src="src"/>
+    <el-image class="rounded" :style="style" :src="src" :preview-src-list="previewSrcList" fit="contain" v-if="preview">
+        <div slot="error" class="text-muted h-100 flex-center">
+            <i class="el-icon-picture-outline"/>
+        </div>
+    </el-image>
+    <el-avatar :size="size" :style="style" icon="el-icon-user-solid" :shape="shape" fit="contain" :src="src" v-else/>
 </template>
 
 <script>
 export default {
     name: 'TnxelAvatar',
     props: {
-        src: String,
+        url: String,
         shape: String,
-        size: {},
+        size: {
+            type: Number,
+            required: true,
+        },
+        preview: {
+            type: Boolean,
+            default: false,
+        }
+    },
+    data() {
+        return {
+            src: null,
+            previewSrcList: null,
+        }
     },
     computed: {
         style() {
-            if (typeof this.size === 'number') {
+            if (this.preview) {
+                return {
+                    width: this.size + 'px',
+                    height: this.size + 'px',
+                    'font-size': (this.size / 2) + 'px',
+                };
+            } else {
                 return {
                     'font-size': (this.size / 2) + 'px',
                 };
             }
-            return undefined;
         }
-    }
+    },
+    created() {
+        if (this.url && this.url.startsWith('fss://')) {
+            let rpc = window.tnx.app.rpc;
+            let options = {
+                app: 'fss'
+            };
+            let vm = this;
+            rpc.ensureLogined(function() {
+                if (vm.preview) {
+                    rpc.get('/meta', {
+                        storageUrl: vm.url
+                    }, function(meta) {
+                        vm.src = meta.thumbnailReadUrl;
+                        vm.previewSrcList = [meta.readUrl];
+                    }, options);
+                } else {
+                    rpc.get('/read-url', {
+                        storageUrl: vm.url,
+                        thumbnail: true,
+                    }, function(readUrl) {
+                        vm.src = readUrl;
+                    }, options);
+                }
+            }, options);
+        }
+    },
 }
 </script>
