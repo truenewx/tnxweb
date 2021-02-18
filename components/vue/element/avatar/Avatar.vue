8097d84b38b561373e30a6a68adb89811fe6ceb8
jianglei <jianglei81@sina.cn>
2021-02-18 18:15:26 +0800

    监听url变化

--- components/vue/element/avatar/Avatar.vue
+++ components/vue/element/avatar/Avatar.vue
@@ -1,5 +1,6 @@
 <template>
-    <el-image class="rounded" :style="style" :src="src" :preview-src-list="previewSrcList" fit="contain" v-if="preview">
+    <el-image :class="shape === 'square' ? 'rounded' : 'rounded-circle'" :style="style" :src="src"
+        :preview-src-list="previewSrcList" fit="contain" v-if="preview">
         <div slot="error" class="text-muted h-100 flex-center">
             <i class="el-icon-picture-outline"/>
         </div>
@@ -37,37 +38,51 @@ export default {
                     'font-size': (this.size / 2) + 'px',
                 };
             } else {
-                return {
+                let style = {
                     'font-size': (this.size / 2) + 'px',
                 };
+                if (this.src) {
+                    style['background-color'] = 'transparent';
+                }
+                return style;
             }
         }
     },
-    created() {
-        if (this.url && this.url.startsWith('fss://')) {
-            let rpc = window.tnx.app.rpc;
-            let options = {
-                app: 'fss'
-            };
-            let vm = this;
-            rpc.ensureLogined(function() {
-                if (vm.preview) {
-                    rpc.get('/meta', {
-                        storageUrl: vm.url
-                    }, function(meta) {
-                        vm.src = meta.thumbnailReadUrl;
-                        vm.previewSrcList = [meta.readUrl];
-                    }, options);
-                } else {
-                    rpc.get('/read-url', {
-                        storageUrl: vm.url,
-                        thumbnail: true,
-                    }, function(readUrl) {
-                        vm.src = readUrl;
-                    }, options);
-                }
-            }, options);
+    watch: {
+        url() {
+            this.load();
         }
     },
+    created() {
+        this.load();
+    },
+    methods: {
+        load() {
+            if (this.url && this.url.startsWith('fss://')) {
+                let rpc = window.tnx.app.rpc;
+                let options = {
+                    app: 'fss'
+                };
+                let vm = this;
+                rpc.ensureLogined(function() {
+                    if (vm.preview) {
+                        rpc.get('/meta', {
+                            storageUrl: vm.url
+                        }, function(meta) {
+                            vm.src = meta.thumbnailReadUrl;
+                            vm.previewSrcList = [meta.readUrl];
+                        }, options);
+                    } else {
+                        rpc.get('/read-url', {
+                            storageUrl: vm.url,
+                            thumbnail: true,
+                        }, function(readUrl) {
+                            vm.src = readUrl;
+                        }, options);
+                    }
+                }, options);
+            }
+        }
+    }
 }
 </script>
