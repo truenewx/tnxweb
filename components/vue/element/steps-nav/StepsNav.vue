1f837d07c7afddd9188842763e24e89e2da8ba9b
jianglei <jianglei81@sina.cn>
2021-03-24 10:44:06 +0800

    首个定位位置不再固定为0而是其实际位置

--- components/vue/element/steps-nav/StepsNav.vue
+++ components/vue/element/steps-nav/StepsNav.vue
@@ -44,7 +44,7 @@ export default {
         navTo(index) {
             this.activeIndex = index;
             let top = 0;
-            if (index > 0) {
+            if (index >= 0) {
                 let top0 = $('#' + this.items[0].target).offset().top;
                 let $target = $('#' + this.items[index].target);
                 top = $target.offset().top - top0 + this.topOffset;
